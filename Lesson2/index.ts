e7bec3b8699faaf9f34fbfc0243e957214c6043c
damir <damir.kalbic@geldgarantie.nl>
2022-07-26 16:39:21 +0200

    Lesson2

---
+++ Lesson2/index.ts
@@ -0,0 +1,16 @@
+// Array
+const LaracastsTeam: string[] = [
+    'Jeffrey Way',
+    'Tudor Adrian',
+    'Ande Madarang'
+]
+
+const listOfNumbers: number[] = [1, 2, 3, 4]
+
+const numbers: Array<number> = [1, 2, 3]
+
+const stringOrNumbers: (string|number)[] = [1, 'hello', 2021, 'Michael']
+
+// Tuples
+const tuple: [number, string] = [2020, 'Why?']
+const wrong: [number, string] = ['why', 2020]
---
+++ Lesson2/lib/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const LaracastsTeam = [
+    'Jeffrey Way',
+    'Tudor Adrian',
+    'Ande Madarang'
+];
+const listOfNumbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3];
+const stringOrNumbers = [1, 'hello', 2021, 'Michael'];
+const tuple = [2020, 'Why?'];
+const wrong = ['why', 2020];
+//# sourceMappingURL=index.js.map
